d6b2978efdbf58c4d07575734bea92f1ecbffd7b
PILean <trinidad.leandro@protonmail.com>
2019-03-31 13:57:17 -0600

    added try catch

--- driveCopy.py
+++ driveCopy.py
@@ -12,21 +12,24 @@ def printlog(text):
     if dev:
         print("[%s]: %s" %(time.asctime( time.localtime(time.time())),text))
 while True:
-    if(os.path.isdir(usb)):
-        copy = []
-        usbFiles = [f for f in os.listdir(usb)]
-        storageFiles = [f for f in os.listdir(storage)]
-        for i in usbFiles:
-            if(i not in storageFiles):
-                copy.append(i)
-        if(copy): # if there is a new file in the usb that is not already in the server
-            printlog(copy)
-            for item in copy:
-                printlog("copying " + item)
-                copyfile(usb + "/" + item, storage + "/" + item)
-                printlog("done")
-        #TODO: gpio lights
-        os.system('sudo eject ' + usbDisk)
-        printlog("Files Copied")
-        time.sleep(10)
+    try:
+        if(os.path.isdir(usb)):
+            copy = []
+            usbFiles = [f for f in os.listdir(usb)]
+            storageFiles = [f for f in os.listdir(storage)]
+            for i in usbFiles:
+                if(i not in storageFiles):
+                    copy.append(i)
+            if(copy): # if there is a new file in the usb that is not already in the server
+                printlog(copy)
+                for item in copy:
+                    printlog("copying " + item)
+                    copyfile(usb + "/" + item, storage + "/" + item)
+                    printlog("done")
+            #TODO: gpio lights
+            os.system('sudo eject ' + usbDisk)
+            printlog("Files Copied")
+            time.sleep(10)
+    except:
+        pass
 
